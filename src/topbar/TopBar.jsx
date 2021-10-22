7aaa3de1e3de016debea71402984c0560e56af27
lggg123 <machinelearner2334@gmail.com>
2021-10-21 20:33:15 -0700

    first commit

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,19 @@
+import "./topbar.css"
+
+export default function TopBar() {
+    return (
+        <div className="top">
+            <div className="topLeft">
+                <h2><span class="logo">Equity</span>smith</h2>
+            </div> 
+            <div className="topRight">
+                <a href="/">Home</a>
+                <a href="/about-us">About Us</a>
+                <a href="/blog">Blog</a>
+                <a href="/become-an-advisor">Become An Advisor</a>
+                <a href="/privacy-policy">Privacy Policy</a>
+                <a href="/contact-us">Contact</a>
+            </div>
+        </div>
+    )
+}
