2f4294b228e4d7e59bde68ee7f969174aaa6b26e
tonsojun <josephjun@gmail.com>
2021-11-03 10:46:31 -0700

    Updated contact.css file

--- src/pages/Financial/Financial.jsx
+++ src/pages/Financial/Financial.jsx
@@ -6,7 +6,7 @@ export default function Financial() {
     const text2 = "The CFP will answer as many of your questions as possible in the time allotted"
     const text3 = "Initial calls are for informational purposes only and the CFP will not attempt to sell you anything - really!"
     const text4 = "At your discretion, you may choose to further explore the services of the professional you speak with the choice is entirely yours!"
-    const text5 = "Equitysmith connects a network of insurance and financial services professionals to our customers. The information conveyed during your call does not constitute paid professional advice and Equitysmith is indemnified from liability of any kind. The use of our service constitutes your acceptance of these terms."
+    const text5 = "Equitysmith connects a network of insurance and financial services professionals to our customers. \nThe information conveyed during your call does not constitute paid professional advice and Equitysmith \nis indemnified from liability of any kind. The use of our service constitutes your acceptance of these terms."
 
     return (
         <>
@@ -20,20 +20,18 @@ export default function Financial() {
                 <div>
                     <h2 className="finance-mini-title2">How your free 15-minute call works:</h2>
                 </div>
-
                 <div className="call-features">
                     <p>{text1}</p>
                     <p>{text2}</p>
                     <p>{text3}</p>
                     <p>{text4}</p>
                 </div>
-
+                <div className="disclaimer">
+                    <p id="grey">{text5}</p>
+                </div>
             </div>
 
 
-            <div>
-                <p id="grey">{text5}</p>
-            </div>
             <img src={aboutUs1}></img>
             <button></button>
             <h1>How can we help you <span id="blue">today?</span></h1>
